f29ec7bac4e85563965c53b700536e9de91c7d20
Matus Horvath <matus.horvath@gmail.com>
2024-03-27 13:28:40 +0100

    Image starts and jumps to init

--- image.asm
+++ image.asm
@@ -1,13 +1,33 @@
 cpu 8086
 
+video_seg   equ 0xb800
+rom_seg     equ 0xf000
 
-section .text start=0xf0000              ; boot
+org (rom_seg * 0x10)
 
-; position the boot code at 0xffff0
-times 0xfff0 db 0
+section video start=(video_seg * 0x10) nobits               ; video memory
 
-loop:
-    jmp loop
+video_off:
+     db  16 dup (?)
 
-; fill in the rest of image up to 0xfffff
-times 0x10000-($-$$) db 0
+
+section rom start=(rom_seg * 0x10)                          ; ROM
+
+init:
+    ; mov ax, video_seg
+    ; mov es, ax
+
+    ; mov byte [es:video_off + 0], 'H'
+    ; mov byte [es:video_off + 1], 0x07
+
+    jmp $
+
+    ; position the boot entry point at 0xffff0
+    ;times 0xfff0-($-$$) db 0
+    db  0xfff0-($-$$) dup 0
+
+boot:                                                       ; boot entry point
+    jmp init
+
+    ; fill in the rest of image up to 0xfffff
+    db  0x10000-($-$$) dup 0
